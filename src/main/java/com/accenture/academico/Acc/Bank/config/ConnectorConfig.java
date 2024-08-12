f7a7be2998000d95871542273171d768ca3516dc
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2024-08-12 14:37:57 -0300

    c

--- src/main/java/com/accenture/academico/Acc/Bank/config/ConnectorConfig.java
+++ src/main/java/com/accenture/academico/Acc/Bank/config/ConnectorConfig.java
@@ -31,9 +31,9 @@ public class ConnectorConfig {
     private Connector getHttpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8081);
+        connector.setPort(8080);
         connector.setSecure(false);
-        connector.setRedirectPort(8080);
+        connector.setRedirectPort(8443);
         return connector;
     }
 }
